c20a33ecb11f05d137edef28c0c6c00dc25f017e
Munchy <Munchy@MOPD716-PC>
2016-10-21 15:04:42 -0500

    Added ex2G

---
+++ mtrumble2g1/Main Form.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblDiscount = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.btnCalcElseIf = New System.Windows.Forms.Button()
+        Me.btnCalcNest = New System.Windows.Forms.Button()
+        Me.btnCalcSelect = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 248)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Price:"
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(81, 271)
+        Me.txtPrice.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(81, 32)
+        Me.txtPrice.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(165, 248)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Quantity:"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(168, 271)
+        Me.txtQuantity.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(81, 32)
+        Me.txtQuantity.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(252, 249)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Discount:"
+        '
+        'lblDiscount
+        '
+        Me.lblDiscount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDiscount.Location = New System.Drawing.Point(255, 274)
+        Me.lblDiscount.Name = "lblDiscount"
+        Me.lblDiscount.Size = New System.Drawing.Size(81, 29)
+        Me.lblDiscount.TabIndex = 7
+        Me.lblDiscount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(338, 249)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 25)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Total due:"
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Location = New System.Drawing.Point(342, 274)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(81, 29)
+        Me.lblTotalDue.TabIndex = 9
+        Me.lblTotalDue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(26, 329)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(98, 32)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(215, 380)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(98, 32)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(53, 38)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 177)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Discount Rates"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(16, 136)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(117, 25)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Over 20:  3%"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(16, 99)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(129, 25)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "16-20:     2.5%"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 65)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(115, 25)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "11-15:     2%"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 28)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 25)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "$0-10:     0"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Location = New System.Drawing.Point(259, 38)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 177)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Sample Results"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(23, 136)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(134, 25)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "$10, Qty 30: $9"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(23, 28)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(134, 25)
+        Me.Label12.TabIndex = 4
+        Me.Label12.Text = "$10, Qty 10: $0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(23, 99)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(134, 25)
+        Me.Label10.TabIndex = 6
+        Me.Label10.Text = "$10, Qty 20: $5"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(23, 65)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(134, 25)
+        Me.Label11.TabIndex = 5
+        Me.Label11.Text = "$10, Qty 15: $3"
+        '
+        'btnCalcElseIf
+        '
+        Me.btnCalcElseIf.Location = New System.Drawing.Point(276, 329)
+        Me.btnCalcElseIf.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalcElseIf.Name = "btnCalcElseIf"
+        Me.btnCalcElseIf.Size = New System.Drawing.Size(128, 32)
+        Me.btnCalcElseIf.TabIndex = 12
+        Me.btnCalcElseIf.Text = "&Calc If-ElseIf"
+        Me.btnCalcElseIf.UseVisualStyleBackColor = True
+        '
+        'btnCalcNest
+        '
+        Me.btnCalcNest.Location = New System.Drawing.Point(130, 329)
+        Me.btnCalcNest.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalcNest.Name = "btnCalcNest"
+        Me.btnCalcNest.Size = New System.Drawing.Size(140, 32)
+        Me.btnCalcNest.TabIndex = 13
+        Me.btnCalcNest.Text = "&Calc Nested If"
+        Me.btnCalcNest.UseVisualStyleBackColor = True
+        '
+        'btnCalcSelect
+        '
+        Me.btnCalcSelect.Location = New System.Drawing.Point(410, 329)
+        Me.btnCalcSelect.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalcSelect.Name = "btnCalcSelect"
+        Me.btnCalcSelect.Size = New System.Drawing.Size(118, 32)
+        Me.btnCalcSelect.TabIndex = 14
+        Me.btnCalcSelect.Text = "&Calc Select "
+        Me.btnCalcSelect.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(550, 431)
+        Me.Controls.Add(Me.btnCalcSelect)
+        Me.Controls.Add(Me.btnCalcNest)
+        Me.Controls.Add(Me.btnCalcElseIf)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblTotalDue)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblDiscount)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Jetters"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtPrice As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtQuantity As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblDiscount As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalDue As System.Windows.Forms.Label
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCalcElseIf As Button
+    Friend WithEvents btnCalcNest As Button
+    Friend WithEvents btnCalcSelect As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
